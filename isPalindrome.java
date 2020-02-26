85afe0174e91e13c7624f83fefb022b148d703be
AiresVM <joaoaires98@gmail.com>
2020-02-26 15:19:04 +0000

    added scanner

--- isPalindrome.java
+++ isPalindrome.java
@@ -1,28 +1,45 @@
+import java.util.Scanner;
+
 public class isPalindrome
 {
     public static boolean isPalindrome(int N, String dada)
     {
-        System.out.println(N);
-        for(int i=1; i<=N;i++)
-        {
         String str = "";//string ao contrario
+
         dada=dada.toLowerCase().replaceAll("[^a-z]" , "");// retirar carateres n letra minuscula
+        
         for(int j=dada.length()-1; j>=0; j--)
         {
            str += dada.charAt(j);
         }
         if(str.equals(dada))
         {
-            System.out.println("sim");;
+            return true;
         }
-        System.out.println("nao");
-        }
-        
+        return false; 
     }
     public static void main(String[] args)
     {
-        
-        isPalindrome(N,String);
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+
+        String str = new String();
+
+        System.out.println(N);
+
+        for(int i=1;i<=N;i++)
+        {
+            str = scan.nextLine();
+            if(isPalindrome(N,str))
+            {
+                System.out.println("sim");
+            }
+            else
+            {
+                System.out.println("nao");
+            }
+        }
+        scan.close();
     }
 }
-
