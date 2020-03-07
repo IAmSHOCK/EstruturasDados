6479c759d703ccb7ffc54004b6296cd9501f683f
AiresVM <joaoaires98@gmail.com>
2020-03-07 12:14:44 +0000

    Rectangle

--- Primes.java
+++ Primes.java
@@ -12,14 +12,15 @@ public class Primes
 	{
 
 		int n=20;
-		//System.out.print(n "= ");
-		for (int i=2; i<=n; i++)
+		System.out.print(n "= ");
+		for (int i=3; i*i<=n; i++)
 		{
 			if (isPrime(i))
 			{
 				if (n%i==0)
 				{
 					n=n/i;
+					System.out.print();
 				}
 			}
 		}
---
+++ src/Point.java
@@ -0,0 +1,16 @@
+class Point 
+{
+   int x, y,z;
+
+   Point() 
+   {
+      x = y = z = 0;
+   }
+   
+   Point(int x0, int y0, int z0) 
+   {
+      x = x0;
+      y = y0;
+      z = z0;
+   }
+}
