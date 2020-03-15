b7ae1ee4abbdbc6fd0cc6b9dd9f99f34cb12fc02
AiresVM <joaoaires98@gmail.com>
2020-03-15 17:54:19 +0000

    GameOfLife done

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -6,58 +6,113 @@
 import java.util.Scanner;
 
 // Classe para representar um jogo
-class Game {
+class Game 
+{
     final char DEAD  = '.';  // Constante que indica uma celula morta
     final char ALIVE = 'O';  // Constante que indica uma celula viva
     private int rows, cols;  // Numero de linhas e colunas
     private char m[][];      // Matriz para representar o estado do jogo
 
     // Construtor: inicializa as variaveis tendo em conta a dimensao dada
-    Game(int r, int c) {
-	rows = r;
-	cols = c;
-	m = new char[r][c];
+    Game(int r, int c) 
+    {
+	   rows = r;
+	   cols = c;
+	   m = new char[r][c];
     }
 
     // Metodo para ler o estado inicial para a matriz m[][]
-    public void read(Scanner in) {
-	for (int i=0; i<rows; i++)
-	    m[i] = in.next().toCharArray();
+    public void read(Scanner in) 
+    {
+	   for (int i=0; i<rows; i++)
+       {
+	       m[i] = in.next().toCharArray();
+       }
     }
     
     // Metodo para escrever a matriz m[][]
-    public void write() {
-	// ... por completar
+    public void write() 
+    {
+	   for (int i=0; i<rows; i++) 
+       {
+           for(int j=0; j<cols; j++)
+           {
+                System.out.print(m[i][j]);    
+           }
+           System.out.println();
+       }
     }
 
+
     // Deve devolver o numero de celulas vivas que sao vizinhas de (y,x)
-    private int countAlive(int y, int x) {
-	int count = 0;
-	// ... por completar
-	return count;
+    private int countAlive(int x, int y) 
+    {
+	   int count = 0;
+	   if(m[x+1][y] == ALIVE && (x+1)<rows)                 count++;
+       if(m[x-1][y] == ALIVE && (x-1)>0)                    count++;
+       if(m[x][y+1] == ALIVE && (y+1)>cols)                 count++;
+       if(m[x][y-1] == ALIVE && (y-1)>0)                    count++;
+       if(m[x+1][y+1] == ALIVE && (x+1)<rows && (y+1)>cols) count++;
+       if(m[x-1][y+1] == ALIVE && (x-1)>0 && (y+1)>cols)    count++;
+       if(m[x+1][y-1] == ALIVE && (x+1)<rows && (y-1)>0)    count++;
+       if(m[x-1][y-1] == ALIVE && (x-1)>0 && (y-1)>0)       count++;
+	   return count;
     }
 
     // Deve fazer uma iteracao: cria nova geracao a partir da actual
-    public void iterate() {
-	// ... por completar
+    public void iterate() 
+    {
+        char[][] proxger = new char[rows][cols];
+	    for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<cols;j++)
+            {
+                if(m[i][j] == ALIVE)
+                {
+                    if(countAlive(i,j) > 3) proxger[i][j]=DEAD;
+                    else 
+                        {
+                            if (countAlive(i,j) < 2) proxger[i][j]=DEAD;
+                            else
+                            {
+                                proxger[i][j]=ALIVE;
+                            }
+                        }
+                }
+                else 
+                    {
+                        if(countAlive(i,j)==3) proxger[i][j]=ALIVE;
+                        else 
+                        {
+                            proxger[i][j]=DEAD;
+                        }
+                    }
+            }
+        }
+        m=proxger;
     }
 
 }
 
 // Classe principal com o main()
-public class ED088 {
-    public static void main(String[] args) {
-	Scanner in = new Scanner(System.in);
+public class ED088 
+{
+    public static void main(String[] args) 
+    {
+	   Scanner in = new Scanner(System.in);
 
-	// Ler linhas, colunas e numero de iteracoes
-	int rows = in.nextInt();
-	int cols = in.nextInt();
-	int n    = in.nextInt();
+	   // Ler linhas, colunas e numero de iteracoes
+	   int rows = in.nextInt();
+	   int cols = in.nextInt();
+	   int n    = in.nextInt();
 
-	// Criar objecto para conter o jogo e ler estado inicial
-	Game g = new Game(rows, cols);
-	g.read(in);
-	
-	//... por completar
+	   // Criar objecto para conter o jogo e ler estado inicial
+	   Game g = new Game(rows, cols);
+	   g.read(in);
+       for(int i=0; i<n; i++)
+       {
+            g.iterate();
+       }
+       g.write();
     }
 }
